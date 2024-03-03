efafa8962a2f72447d3615e25d42fdcfafefdfca
wzyndul <242575@edu.p.lodz.pl>
2024-03-03 19:18:30 +0100

    feat: basic page for results

--- sentiment_analysis/sentiment_app/views.py
+++ sentiment_analysis/sentiment_app/views.py
@@ -6,6 +6,18 @@ def main_view(request):
 
 
 def analysis_view(request):
-    # Add your logic for analyzing YouTube comments here
-    return render(request, 'analysis.html')
+    platform = None
+    if request.method == 'POST':
+        if 'x_username' in request.POST:
+            x_username = request.POST.get('x_username')
+            platform = 'X'
+        elif 'youtube_link' in request.POST:
+            youtube_link = request.POST.get('youtube_link')
+            platform = 'Youtube'
+        context = {
+            'platform': platform,
+            'x_username': x_username if platform == 'X' else None,
+            'youtube_link': youtube_link if platform == 'Youtube' else None,
+        }
 
+    return render(request, 'analysis.html', context)
