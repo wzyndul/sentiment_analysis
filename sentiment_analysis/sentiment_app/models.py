59662d1da1e1778609be176dbbf460734ce885cb
wzyndul <242575@edu.p.lodz.pl>
2024-04-05 14:15:20 +0200

    feat: model for creator and video

--- sentiment_analysis/sentiment_app/models.py
+++ sentiment_analysis/sentiment_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    channel_id = models.ForeignKey('Creator', on_delete=models.CASCADE)
+    channel_name = models.CharField(max_length=100)
+    url = models.URLField()
+    title = models.CharField(max_length=100)
+    time_published = models.DateTimeField()
+    num_comments = models.IntegerField()
+    positive = models.IntegerField()
+    negative = models.IntegerField()
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+class Creator(models.Model):
+    channel_id = models.CharField(max_length=100)
+    channel_name = models.CharField(max_length=100)
+    picture_url = models.URLField()
+
+    def __str__(self):
+        return self.channel_name
